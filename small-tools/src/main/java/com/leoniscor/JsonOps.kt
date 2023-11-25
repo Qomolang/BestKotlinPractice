b4cb2cb005f05c44a6dfbb93072a0da82bf625ae
Qomolang <84028136@qq.com>
2023-11-25 23:29:10 +0800

    更新jsonOps

---
+++ small-tools/src/main/java/com/leoniscor/JsonOps.kt
@@ -0,0 +1,19 @@
+package com.leoniscor
+
+/**
+ * @author gaosong
+ */
+class JsonOps {
+
+
+    /**
+     * 比较两个json是否相同，第一步，将两个json的key按照相同顺序排序
+     * 注意：依赖fastjson
+     */
+//    fun sortJsonByKey(input: String) {
+//        var jsonObject = JSON.parseObject(input);
+//        input = jsonObject.toString(SerializerFeature.MapSortField);
+//        println("input = " + input);
+//    }
+
+}
